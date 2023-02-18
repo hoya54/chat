966b6179dfa2c732e3135c08b9db3bcdbdadb638
hoya445 <hoya445@naver.com>
2023-02-18 21:53:38 +0900

    .

--- backend/chat/src/main/java/mpti/common/config/WebSocketConfig2.java
+++ backend/chat/src/main/java/mpti/common/config/WebSocketConfig2.java
@@ -1,7 +1,6 @@
 package mpti.common.config;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.WebSocketHandler;
@@ -11,7 +10,7 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 
 @Configuration
 @EnableWebSocket
-public class WebSocketConfig implements WebSocketConfigurer {
+public class WebSocketConfig2 implements WebSocketConfigurer {
 
     @Bean
     public WebSocketHandler myHandler() {
