45cffe4c5629c146b4d8288d08c569ca85eecb9d
hoya445 <hoya445@naver.com>
2023-02-19 00:19:19 +0900

    .

--- backend/chat/src/main/java/mpti/common/config/WebSocketConfig3.java
+++ backend/chat/src/main/java/mpti/common/config/WebSocketConfig3.java
@@ -1,29 +1,29 @@
-package mpti.common.config;
+//package mpti.common.config;
+////
 //
-
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-
-@Slf4j
-@Configuration
-@EnableWebSocketMessageBroker
-public class WebSocketConfig3 extends AbstractWebSocketMessageBrokerConfigurer {
-
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/api/chat", "/groupChat");
-        config.setApplicationDestinationPrefixes("/api/chat");
-        config.setUserDestinationPrefix("http://localhost:8005/api/chat");
-    }
-
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        log.info("gs-websocket socket");
-        registry.addEndpoint("http://localhost:8005/api/chat").withSockJS();
-    }
-
-}
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+//import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
+//import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+//import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+//
+//@Slf4j
+//@Configuration
+//@EnableWebSocketMessageBroker
+//public class WebSocketConfig3 extends AbstractWebSocketMessageBrokerConfigurer {
+//
+//    @Override
+//    public void configureMessageBroker(MessageBrokerRegistry config) {
+//        config.enableSimpleBroker("/api/chat", "/groupChat");
+//        config.setApplicationDestinationPrefixes("/api/chat");
+//        config.setUserDestinationPrefix("http://localhost:8005/api/chat");
+//    }
+//
+//    @Override
+//    public void registerStompEndpoints(StompEndpointRegistry registry) {
+//        log.info("gs-websocket socket");
+//        registry.addEndpoint("http://localhost:8005/api/chat").withSockJS();
+//    }
+//
+//}
