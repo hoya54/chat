7d024aca997f027ef2e9f0aa8464575426b4b4a7
hoya445 <hoya445@naver.com>
2023-02-18 22:41:50 +0900

    .

---
+++ backend/chat/src/main/java/mpti/common/config/ChatConfiguration.java
@@ -0,0 +1,22 @@
+package mpti.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class ChatConfiguration extends AbstractWebSocketMessageBrokerConfigurer {
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+//        registry.enableSimpleBroker("/chat", "/participants");
+//        registry.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry stompEndpointRegistry) {
+        stompEndpointRegistry.addEndpoint("/api/cjat").withSockJS();
+    }
+}
