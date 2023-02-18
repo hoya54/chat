d76bee6c303da1c6c637e06f4499cf32958c7719
hoya445 <hoya445@naver.com>
2023-02-18 21:47:10 +0900

    .

---
+++ backend/chat/src/main/java/mpti/common/config/WebSocketConfig.java
@@ -0,0 +1,25 @@
+package mpti.common.config;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    @Bean
+    public WebSocketHandler myHandler() {
+        return new MyHandler();
+    }
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(myHandler(), "/api/chat").setAllowedOrigins("*").withSockJS();
+    }
+}
