65182c18273bf9baf87faa95ebcc4fed88c8ba72
hoya445 <hoya445@naver.com>
2023-02-18 22:01:44 +0900

    .

--- backend/chat/src/main/java/mpti/common/config/WebSocketConfig2.java
+++ backend/chat/src/main/java/mpti/common/config/WebSocketConfig2.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
@@ -21,4 +22,10 @@ public class WebSocketConfig2 implements WebSocketConfigurer {
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(myHandler(), "/api/chat").setAllowedOrigins("*").withSockJS();
     }
+
+//    @Override
+//    public void registerStompEndpoints(StompEndpointRegistry registry) {
+//        // WebSocketMessageBrokerConfigurer.super.registerStompEndpoints(registry);
+//        registry.addEndpoint("/api/chat").setAllowedOrigins("*").withSockJS();
+//    }
 }
