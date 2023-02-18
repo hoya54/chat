3647695e1cd34329e67c2cc5dee9f023ef9f5d61
hoya445 <hoya445@naver.com>
2023-02-19 00:22:40 +0900

    .

--- backend/chat/src/main/java/mpti/common/config/StompWebSocketConfig.java
+++ backend/chat/src/main/java/mpti/common/config/StompWebSocketConfig.java
@@ -24,7 +24,7 @@ public class StompWebSocketConfig implements WebSocketMessageBrokerConfigurer {
     /*어플리케이션 내부에서 사용할 path를 지정할 수 있음*/
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.setApplicationDestinationPrefixes("/receive");
-        registry.enableSimpleBroker("/sub");
+//        registry.setApplicationDestinationPrefixes("/receive");
+//        registry.enableSimpleBroker("/sub");
     }
 }
